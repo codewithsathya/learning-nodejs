14b233c1225bd1902eac4c2aae2ce09aac01157f
S Sathiya <codewithsathya@gmail.com>
2021-07-08 10:05:55 +0530

    Module wrapper function

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,8 +1,13 @@
-let url = "https://logger.io/log";
+(function (exports, require, module, __filename, __dirname) { //IIFE
+  let url = "https://logger.io/log";
 
-function log(message) {
-  console.log(message);
-}
+  function log(message) {
+    console.log(message);
+  }
 
-module.exports.log = log;
+  module.exports = log;
+  module.exports.log = log;
+  exports.log = log;
+  //exports = log; //produces error
+})
 //module.exports = log; takes function instead of object
