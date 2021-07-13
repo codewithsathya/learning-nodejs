7a9e690777591a8fdbf278a2927ee8aa7ffb7af3
S Sathiya <codewithsathya@gmail.com>
2021-07-13 20:30:12 +0530

    Publishing a package

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,7 +1,11 @@
 let _ = require('underscore');
+let {add} = require('lion-add-num');
 // Core module
 // File of folder
 // node_modules
 
 let result = _.contains([1, 2, 3], 3);
 console.log(result);
+
+let sum = add(5, 6);
+console.log(sum);
