066f9e75bb407f927412ff6f344509cdc5720751
S Sathiya Seelan <codewithsathya@gmail.com>
2021-08-11 09:17:21 +0530

    Input validation

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const Joi = require("joi");
 
-app.use(express.json())
+app.use(express.json());
 
 const courses = [
-  {id: 1, name: 'course1'},
-  {id: 2, name: 'course2'},
-  {id: 3, name: 'course3'},
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -18,19 +19,31 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if(!course) res.status(404).send("The course with given ID was not found");
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with given ID was not found");
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+  console.log(result);
+
+  if(result.error) {
+    res.status(400).send(result.error);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
-    name: req.body.name
-  }
+    name: req.body.name,
+  };
   courses.push(course);
   res.send(course);
-})
+});
 
 const port = process.env.PORT || 3000;
 console.log(port);
